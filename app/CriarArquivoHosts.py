073f09d353c41ad60d3dd1ad81331697906ea9f7
padilhafe <felipedpadilha@gmail.com>
2024-10-10 21:35:41 -0300

    feat: melhorias no processo de criacao

--- app/CriarArquivoHosts.py
+++ app/CriarArquivoHosts.py
@@ -1,5 +1,5 @@
-def CriarArquivoHosts(arquivo, dominios, subdominios):
-    with open(arquivo, "w") as file:
+def CriarArquivoHosts(arquivo, modo, dominios, subdominios):
+    with open(arquivo, modo) as file:
         for dominio in dominios:
             for subdominio in subdominios['subdominios']:
                 file.write(f"127.0.0.0.1 {subdominio}.{dominio}\n")
--- main.py
+++ main.py
@@ -17,13 +17,36 @@ print("""
 while True:
     opcao = MenuPrincipal()
     if opcao == 1:
+        with open(CAMINHO_ARQUIVO_HOSTS_GERAL, "r") as file:
+            hosts = file.readlines()
+            file.close()
+        
+        if hosts:
+            print("Arquivo hosts já existe. O que deseja fazer?\n")
+            resposta = int(input("1. Sobrescrever\n2. Adicionar ao final\n3. Exibir conteúdo\n0. Cancelar operação\n > "))
+            if resposta == 1:
+                modo = "w"
+            elif resposta == 2:
+                modo = "a"
+            elif resposta == 3:
+                print("Conteúdo do arquivo hosts:")
+                for host in hosts:
+                    print(host.strip())
+                continue
+            elif resposta == 0:
+                print("Operação cancelada.\n")
+                continue
+            else:
+                print("Opção inválida! Operação cancelada!\n")
+                continue
+
         with open(CAMINHO_ARQUIVO_SUBDOMINIOS, "r") as file:
             subdominios = json.load(file)
             file.close()
 
         dominios = InputDominio()
         
-        CriarArquivoHosts(CAMINHO_ARQUIVO_HOSTS_GERAL, dominios, subdominios)
+        CriarArquivoHosts(CAMINHO_ARQUIVO_HOSTS_GERAL, modo, dominios, subdominios)
 
     elif opcao == 2:
         print("Qual arquivo deseja editar?")
