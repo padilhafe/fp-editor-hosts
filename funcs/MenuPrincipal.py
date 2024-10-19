2a505ac80ca6d359d4ab03a601159ba86a5b1182
padilhafe <felipedpadilha@gmail.com>
2024-10-19 10:55:11 -0300

    ajustes no menu principal

--- funcs/MenuPrincipal.py
+++ funcs/MenuPrincipal.py
@@ -1,5 +1,5 @@
 def MenuPrincipal():
-    print("1. Criar novo arquivo de hosts")
+    print("1. Editar arquivo de hosts")
     #print("2. Editar um arquivo de hosts existente")
     #print("3. Listar subdominios")
     #print("4. Adicionar novo subdominio")
