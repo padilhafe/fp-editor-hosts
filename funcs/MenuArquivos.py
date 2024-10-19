dd11c8ece0abbf7b033314cf152fe43628735fb3
padilhafe <felipedpadilha@gmail.com>
2024-10-19 10:35:00 -0300

    suporte a edicao de multiplos arquivos

--- funcs/MenuArquivos.py
+++ funcs/MenuArquivos.py
@@ -3,18 +3,25 @@ def MenuArquivos(arquivos):
         print("Selecione uma das opções:")
         for i, chave in enumerate(arquivos.keys(), 1):
             print(f"{i}. {chave}")
+        print("99. Todos os arquivos.")
+        print("0. Cancelar operação.")
 
-        print("0. Voltar")
+        opcao = int(input("> "))
+
+        if opcao == 0:
+            print("Operação cancelada.")
+            exit()
+
+        if opcao == 99:
+            return arquivos
 
         try:
-            opcao = int(input("> "))
             if 1 <= opcao <= len(arquivos):
                 chave_selecionada = list(arquivos.keys())[opcao - 1]
                 return arquivos[chave_selecionada]
-            elif opcao == 0:
-                print("Operação cancelada.")
-                exit()
+            
             else:
                 print("Opção inválida! Tente novamente.")
+
         except ValueError:
             print("Entrada inválida! Digite um número válido.")
--- main.py
+++ main.py
@@ -36,41 +36,82 @@ while True:
     opcao = MenuPrincipal()
     if opcao == 1:
         caminho = MenuArquivos(hosts_files)
-        
-        try:
-            with open(caminho, "r") as file:
-                hosts = file.readlines()
-                file.close()
-        except FileNotFoundError:
-            hosts = []
-            modo = "w"
-        
-        if hosts:
-            print("Arquivo hosts já existe. O que deseja fazer?\n")
-            resposta = int(input("1. Sobrescrever\n2. Adicionar ao final\n3. Exibir conteúdo\n0. Cancelar operação\n > "))
-            if resposta == 1:
+
+        if type(caminho) is str:
+            try:
+                with open(caminho, "r") as file:
+                    hosts = file.readlines()
+                    file.close()
+            
+            except FileNotFoundError:
+                hosts = []
                 modo = "w"
-            elif resposta == 2:
-                modo = "a"
-            elif resposta == 3:
-                print("Conteúdo do arquivo hosts:")
-                for host in hosts:
-                    print(host.strip())
-                continue
-            elif resposta == 0:
-                print("Operação cancelada.\n")
-                continue
-            else:
-                print("Opção inválida! Operação cancelada!\n")
-                continue
+            
+            if hosts:
+                print("Arquivo hosts já existe. O que deseja fazer?\n")
+                resposta = int(input("1. Sobrescrever\n2. Adicionar ao final\n3. Exibir conteúdo\n0. Cancelar operação\n > "))
+                if resposta == 1:
+                    modo = "w"
+                elif resposta == 2:
+                    modo = "a"
+                elif resposta == 3:
+                    print("Conteúdo do arquivo hosts:")
+                    for host in hosts:
+                        print(host.strip())
+                    continue
+                elif resposta == 0:
+                    print("Operação cancelada.\n")
+                    continue
+                else:
+                    print("Opção inválida! Operação cancelada!\n")
+                    continue
 
-        with open(CAMINHO_ARQUIVO_SUBDOMINIOS, "r") as file:
-            subdominios = json.load(file)
-            file.close()
+            with open(CAMINHO_ARQUIVO_SUBDOMINIOS, "r") as file:
+                subdominios = json.load(file)
+                file.close()
 
-        dominios = InputDominio()
-        
-        CriarArquivoHosts(caminho, modo, dominios, subdominios)
+            dominios = InputDominio()
+            
+            CriarArquivoHosts(caminho, modo, dominios, subdominios)
+
+        else:
+            for setor, arquivo in caminho.items():
+                print(f"Alterando o arquivo hosts do setor: {setor}.")
+                try:
+                    with open(arquivo, "r") as file:
+                        hosts = file.readlines()
+                        file.close()
+            
+                except FileNotFoundError:
+                    hosts = []
+                    modo = "w"
+                
+                if hosts:
+                    print("Arquivo hosts já existe. O que deseja fazer?\n")
+                    resposta = int(input("1. Sobrescrever\n2. Adicionar ao final\n3. Exibir conteúdo\n0. Cancelar operação\n > "))
+                    if resposta == 1:
+                        modo = "w"
+                    elif resposta == 2:
+                        modo = "a"
+                    elif resposta == 3:
+                        print("Conteúdo do arquivo hosts:")
+                        for host in hosts:
+                            print(host.strip())
+                        continue
+                    elif resposta == 0:
+                        print("Operação cancelada.\n")
+                        continue
+                    else:
+                        print("Opção inválida! Operação cancelada!\n")
+                        continue
+
+                with open(CAMINHO_ARQUIVO_SUBDOMINIOS, "r") as file:
+                    subdominios = json.load(file)
+                    file.close()
+
+                dominios = InputDominio()
+                
+                CriarArquivoHosts(arquivo, modo, dominios, subdominios)
 
     elif opcao == 0:
         print("Encerrando...")
