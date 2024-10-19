7e045efc614b86b8ab5d0c71460530b07be696bd
padilhafe <felipedpadilha@gmail.com>
2024-10-19 09:46:51 -0300

    opcao de voltar

--- funcs/MenuArquivos.py
+++ funcs/MenuArquivos.py
@@ -4,11 +4,16 @@ def MenuArquivos(arquivos):
         for i, chave in enumerate(arquivos.keys(), 1):
             print(f"{i}. {chave}")
 
+        print("0. Voltar")
+
         try:
             opcao = int(input("> "))
             if 1 <= opcao <= len(arquivos):
                 chave_selecionada = list(arquivos.keys())[opcao - 1]
                 return arquivos[chave_selecionada]
+            elif opcao == 0:
+                print("Operação cancelada.")
+                return None
             else:
                 print("Opção inválida! Tente novamente.")
         except ValueError:
